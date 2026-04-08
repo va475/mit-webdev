b459fa52204c6782cda667777a633118eab7495c
Varad Uplenchwar <varaduplenchwar@gmail.com>
2026-04-08 18:38:26 +0530

    Initials of JavaScript

---
+++ phase-1/week-1/script.js
@@ -0,0 +1,21 @@
+let myName = "Varad";
+let myAge = 20;
+let isLearning = true;
+
+console.log("My name is " + myName);
+console.log("My age is " + myAge);
+console.log("Am I learning ? " + isLearning);
+
+
+function sayHello() {
+    alert("Hello!! I am " + myName);
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = "#ff66000";
+
+}
+
+function resetColor() {
+    document.body.style.backgroundColor = "#f0f4f8";
+}
